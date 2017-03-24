0ae2e0eb15dc8d0eb41df00e53f659918636fa5a
wangx <1085518932@qq.com>
2017-03-24 09:14:09 +0800

    newhtml

--- js/script.js
+++ js/script.js
@@ -218,7 +218,105 @@ $.fn.extend({
                 });
             });
         });
+    },
+    fullPage:function (options) {
+        var defaults={
+            ele:['.ease_header','.control']
+        };
+        options=$.extend({},defaults,options);
+        return this.each(function () {
+            var H=0;
+            $.each(options.ele,function (index,ele) {
+                H+=$(ele).outerHeight(true)
+            });
+            var full=$(window).height()-H;
+            $(this).height(full);
+        });
+    },
+    floorTree:function (options) {
+        var defaults={
+
+        };
+        options=$.extend({},defaults,options);
+        return this.each(function () {
+            var _this=$(this);
+            $(this).find('.switch').on('click',function () {
+                if($(this).is('.on')){
+                    $(this).removeClass('on').addClass('off');
+                }else{
+                    $(this).removeClass('off').addClass('on');
+                }
+                $(this).siblings('ul').slideToggle();
+            });
+
+        });
+    },
+    leftSlide:function (options) {
+        var defaults={
+            value:'-218px'
+        };
+        options=$.extend({},defaults,options);
+        return this.each(function () {
+            $(this).on('click',function () {
+                var _this=$(this);
+                if(_this.is('.off')){
+                    _this.removeClass('off').addClass('on');
+                    _this.parent().animate({right:0});
+                }else{
+                    _this.removeClass('on').addClass('off');
+                    _this.parent().animate({right:options.value});
+                }
+            });
+        });
+    },
+    drag:function (options) {
+        var defaults={
+            container:'.fullPhotoRect'
+        };
+        options=$.extend({},defaults,options);
+        return this.each(function () {
+            var _this=$(this);
+            var starX=0,starY=0;
+            var handleWidth=_this.outerWidth(true),containerWidth=$(options.container).width();
+            var handleHeight=_this.outerHeight(true),containerHeight=$(options.container).height();
+            var right=0,bottom=0;
+            _this.on('mousedown',function (event) {
+                right=parseInt(_this.css('right'));
+                bottom=parseInt(_this.css('bottom'));
+                starX=event.pageX;
+                starY=event.pageY;
+                $(document).on('mousemove',move);
+            });
+            _this.on('mouseup',function (event) {
+                $(document).off('mousemove',move);
+            });
+            $(document).on('mouseup',function () {
+                $(document).off('mousemove',move);
+            });
+            function move(event) {
+                window.getSelection?window.getSelection().removeAllRanges():document.selection.empty();
+                var moveX=event.pageX,moveY=event.pageY;
+                var offsetRight=starX-moveX+right,offsetBottom=starY-moveY+bottom;
+                if(offsetRight<0){
+                    offsetRight=0
+                }else if((offsetRight+handleWidth)>containerWidth){
+                    offsetRight=containerWidth-handleWidth;
+                }else{
+                    offsetRight=offsetRight;
+                }
+                if(offsetBottom<0){
+                    offsetBottom=0
+                }else if((offsetBottom+handleHeight)>containerHeight){
+                    offsetBottom=containerHeight-handleHeight;
+                }else{
+                    offsetBottom=offsetBottom;
+                }
+                _this.css({right:offsetRight,bottom:offsetBottom});
+            }
+
+        });
     }
+    
 });
 
 var artDialogs = function (obj) {
@@ -226,6 +324,7 @@ var artDialogs = function (obj) {
         lock: obj.lock ? obj.lock : true,
         drag: obj.drag ? obj.drag : false,
         time: obj.time ? obj.time : null,
+        width:obj.width? obj.width:'auto',
         background: obj.background ? obj.background : '#fff',
         opacity: obj.opacity ? obj.opacity : 0.6,
         padding: obj.padding ? obj.padding : '18px',
