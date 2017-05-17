3f59b807769410fdd52873089594e115cf15cc3f
wangx <1085518932@qq.com>
2017-05-17 09:12:25 +0800

    更改的首页UI

---
+++ easy_edit/js/footer.js
@@ -0,0 +1,6 @@
+document.writeln("<div class=\'footer bg2\'>");
+document.writeln("        <div class=\'pd20 clearfix\'>");
+document.writeln("            <p class=\'fl\'>Copyright© 2016 huijiaoyun.com. All rights reserved 鄂ICP备13001992号</p>");
+document.writeln("            <p class=\'fr\'><span>客服热线：6546546546</span><span>QQ：400888888转1</span></p>");
+document.writeln("        </div>");
+document.writeln("</div>");
